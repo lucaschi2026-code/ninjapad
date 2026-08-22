fd7ab46d11606183f4fa468d46fa6f02fe4e20c2
Lucas Chi <lucas.chi2026@gmail.com>
2026-08-22 11:53:38 -0400

    Disable console output for debugging
    Removed console output enablement for debugging.

--- Firmware/rules.mk
+++ Firmware/rules.mk
@@ -1,7 +1,3 @@
-# Enables qmk console output for debugging.
-# Remove or set to 'no' once debugging is finished.
-CONSOLE_ENABLE = yes
-
 # 0.91" SSD1306 OLED over I2C.
 OLED_ENABLE = yes
 OLED_DRIVER = ssd1306
